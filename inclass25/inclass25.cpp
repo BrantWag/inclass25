610d28c10e41e9f98d28bd9e4ece060075c89ab4
BRANT WAGNER <brawagne@PLAZ05>
2018-01-25 13:03:59 -0700

    second push

--- inclass25/inclass25.cpp
+++ inclass25/inclass25.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
 	srand(static_cast<unsigned int>(time(0))); //seed random generator
-	int secertNumber = rand() % 100 + 1; //Random number between 1 and 100
+	int secertNumber = rand() % 100 + 1; //Random number between 1 and 64
 	int tries = 0;
 	int guess;
 	cout << "\tWelcome to the Guess My Number\n\n";
@@ -19,12 +19,21 @@ int main()
 	{
 		cout << "Enter a guess:";
 		cin >> guess;
-		++tries
+		++tries;
 		
-
-	
-	
-	}
+		if (guess > secertNumber)
+		{
+			cout << "Too high!\n\n";
+		}
+		else if (guess < secertNumber)
+		{
+			cout << "Too low!\n\n";
+		}
+		else
+		{
+			cout << "\nThat's it! You got it in" << tries << "guesses!\n";
+		}
+	} while (guess != secertNumber);
 
     return 0;
 }
